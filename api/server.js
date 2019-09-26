ffdd1f70a18b31f0b3a73488d324d4b9db237f06
TheLessonHere <48539448+TheLessonHere@users.noreply.github.com>
2019-09-26 10:37:33 -0600

    Survey added to different router

--- api/server.js
+++ api/server.js
@@ -9,6 +9,7 @@ const userRouter = require('../routers/users/user-router');
 const swipeRouter = require('../routers/swipes/swipe-router');
 const messageRouter = require('../routers/messages/message-router');
 const friendsRouter = require('../routers/friends/friends-router');
+const surveyRouter = require('../routers/survey/survey-router');
 
 server.use(helmet());
 server.use(cors());
@@ -16,6 +17,7 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/user', authenticate, userRouter);
+server.use('/api/survey', surveyRouter);
 server.use('/api/swipe', authenticate, swipeRouter);
 server.use('/api/messages', authenticate, messageRouter);
 server.use('/api/friends', authenticate, friendsRouter);
---
+++ routers/survey/survey-router.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const db = require('../../auth/helpers');
+
+const { validateUserId, checkUserCreds } = require('../../auth/middleware');
+
+// Update user
+
+router.put('/:user_id', validateUserId, checkUserCreds, (req, res) => {
+    const { user_id } = req.params;
+    db.updateUser(user_id, req.body)
+    .then(user => {
+        if(user) {
+            res.status(201).json({ message: "User updated successfully" });
+        } else{
+            res.status(500).json({ error: "Server error, no users updated" });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "Server error updating user" })
+    });
+});
+
+module.exports = router;
